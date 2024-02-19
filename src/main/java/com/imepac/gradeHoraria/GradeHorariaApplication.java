7af0af136637d2174e33de404fdaeaec1b2da046
Marcos Paulo <marcospaulomacedo@gmail.com>
2024-02-19 14:09:50 -0300

    update projeto

---
+++ src/main/java/com/imepac/gradeHoraria/GradeHorariaApplication.java
@@ -0,0 +1,12 @@
+package com.imepac.gradeHoraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeHorariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeHorariaApplication.class, args);
+	}
+}
